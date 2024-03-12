baf8f9a27262966255d78dae330455e97046cf25
Abhishek Kumar <abhi24033c@gmail.com>
2024-03-12 21:10:24 +0530

    Privacy policy and term & condition added

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,27 @@
 
 
 
+import { BrowserRouter, Route, Routes } from "react-router-dom"
 import Home from "./pages/Home"
+import PrivacyPolicy from "./pages/PrivacyPolicy"
+import Navbar from "./components/Navbar"
+import Term_Condition from "./pages/Term_Condition"
 
 function App() {
- 
+
 
   return (
     <>
-      <Home/>
+    {/* <Navbar/> */}
+      <BrowserRouter>
+      <Navbar/>
+        <Routes >
+          <Route path="/" element={<Home />} />
+          <Route path="/privacy_policy" element={<PrivacyPolicy />} />
+          <Route path="/term_condition" element={<Term_Condition />} />
+        </Routes>
+      </BrowserRouter>
+
     </>
   )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,17 @@
 import React, { useState } from 'react';
 
 import "../css/header.css";
+import { Link } from 'react-router-dom';
 const Navbar = () => {
-    const [openNav,setOpenNav]=useState(false);
+    const [openNav, setOpenNav] = useState(false);
+    const [activeNav, setActiveNav] = useState('home');
 
-    function toggleNav(){
+    function toggleNav() {
         setOpenNav(!openNav);
+    }
+
+    function activeNavLink(nav) {
+        setActiveNav(nav);
     }
 
     return (
@@ -14,14 +20,14 @@ const Navbar = () => {
                 <h1>E- commerce.</h1>
                 <nav className={`nav ${openNav ? `toggle` : ``}`}>
                     <ul>
-                        <li><a href="#home" className="active-className">Home</a></li>
-                        <li><a href="#chefs" >Privacy Policy</a></li>
-                        <li><a href="#chefs" >Terms  & Conditions</a></li>
-                        <li><a href="#contact" >Contact</a></li>
+                        <li><Link to={'/'} className={`${activeNav === "home" ? "active-class" : ""}`} onClick={() => activeNavLink("home")}>Home</Link></li>
+                        <li><Link to={'/privacy_policy'} className={`${activeNav === "privacy_policy" ? "active-class" : ""}`} onClick={() => activeNavLink("privacy_policy")}>Privacy Policy</Link></li>
+                        <li><Link to={'/term_condition'} className={`${activeNav === "term_condition" ? "active-class" : ""}`} onClick={() => activeNavLink("term_condition")}>Terms  & Conditions</Link></li>
+                        {/* <li><a href="#contact" >Contact</a></li> */}
                     </ul>
                 </nav>
                 <div className="buttons">
-                    <i className={`fa-solid ${openNav?"fa-xmark":"fa-bars"}`} id="bars" onClick={toggleNav}></i>
+                    <i className={`fa-solid ${openNav ? "fa-xmark" : "fa-bars"}`} id="bars" onClick={toggleNav}></i>
                 </div>
 
             </header>
